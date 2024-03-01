fc481f83eecd509bec86a1baa296ecbcf6d826de
shadmeoli <shadrack@shopzetu.com>
2024-03-01 17:01:36 +0300

    update: counting the numbe of time a value is repeated in a range of values

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func finder(r int, target int) int {
+	var count int
+	for i := 0; i <= r; i++ {
+		if strings.Contains(strconv.Itoa(i), strconv.Itoa(target)) {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(finder(100, 9))
+}
