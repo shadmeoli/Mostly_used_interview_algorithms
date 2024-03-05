fcd1e222bab640f3d1679b15c7078c7284a8334b
shadmeoli <shadrack@shopzetu.com>
2024-03-05 19:37:12 +0300

    update: go bubble sort clean up

--- go-lang/main.go
+++ go-lang/main.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 )
 
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = temp
+			}
+		}
+	}
+}
+
 func main() {
 	arr := [4]int{1, 3, 2, 4}
-	bubble := bubbleSort(arr)
-	fmt.Println(bubble)
+	// bubble := bubbleSort(arr)
+	// fmt.Println(bubble)
+	fmt.Println(arr)
 }
