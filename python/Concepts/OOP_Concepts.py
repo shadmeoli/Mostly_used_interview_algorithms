a40cb1f0fcbac0c79dfdc9074809188e47ea4a25
shadmeoli <shadrack@shopzetu.com>
2024-04-14 12:00:00 +0300

    [update] class based decorator pattern

---
+++ python/Concepts/OOP_Concepts.py
@@ -0,0 +1,48 @@
+"""
+I am trying to learn the concepts of:
+    [ ] dunder methods
+    [ ] cls methods
+    [ ] self methods
+    [ ] walrus methods
+    [ ] how to avoid reference counting and also circular imports
+    [ ] Decorator pattern with classes
+    [ ] Instance and class methods
+
+Most of these are OOP concepts which I would be also using a more 
+OOP centered language like C++, C# of Java
+"""
+from typing import Callable
+class Test1:
+    ...
+
+class Test2:
+    ...
+
+class Deorator:
+
+    def __init__(self, func: Callable, *args, **kwargs):
+        self.func = func
+        self.__args = args
+        self.__kwargs = kwargs
+
+    def __call__(self):
+
+        result, __memoized = self.__memoize(self.__args, self.__kwargs)
+        def wrapper_function():
+            self.func(result)
+        return wrapper_function()
+
+    def __memoize(self, *args, **kwargs):
+        match (self.__args, self.__kwargs):
+            case(self.__args):
+                return self.__args, False
+            case(self.__kwargs):
+                return self.__kwargs, False
+
+        return None, True
+
+
+"""
+Using the with key word for auto closing open sessions
+and file readers/db sessions for better later access
+"""
