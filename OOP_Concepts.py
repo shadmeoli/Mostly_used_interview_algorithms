628c37004d86df84a73e34ee81560c7b82cc956e
shadmeoli <shadrack@shopzetu.com>
2024-04-15 19:43:12 +0300

    [update] learning some concepts of oop

---
+++ OOP_Concepts.py
@@ -0,0 +1,24 @@
+"""
+I am trying to learn the concepts of:
+    [ ] dunder methods
+    [ ] cls methods
+    [ ] self methods
+    [ ] walrus methods
+    [ ] how to avoid reference counting and also circular imports
+    [ ] Decorator pattern with classes
+    [ ] Instance and class methods
+
+Most of these are OOP concepts which I would be also using a more 
+OOP centered language like C++, C# of Java
+"""
+
+class Test1:
+    ...
+
+class Test2:
+    ...
+
+class Deorator:
+    ...
+
+
