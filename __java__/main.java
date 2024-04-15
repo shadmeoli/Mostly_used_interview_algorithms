de525007fcdbb2e80da3273bd4c331eac2368a17
shadmeoli <shadrack@shopzetu.com>
2024-04-15 19:47:58 +0300

    [update] learning some concepts of oop

---
+++ __java__/main.java
@@ -0,0 +1,14 @@
+/**
+ * I am trying to learn the concepts of:
+ * [ ] dunder methods
+ * [ ] cls methods
+ * [ ] self methods
+ * [ ] walrus methods
+ * [ ] how to avoid reference counting and also circular imports
+ * [ ] Decorator pattern with classes
+ * [ ] Instance and class methods
+ */
+
+public static void main(String[] args) {
+    System.out.println("Learning OOP with java");
+}
