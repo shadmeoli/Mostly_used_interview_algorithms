bcff0b1a960c3356ed5f1c7f3c03de9f296537f4
shadmeoli <shadcodes@gmail.com>
2025-07-24 10:32:10 +0300

    update: traversing question2

--- python/problems/traversing_arrays.py
+++ python/problems/traversing_arrays.py
@@ -7,6 +7,7 @@ If no such element exists, return -1.
 from python.tester.testLogger import test_runner
 
 
+# level 1
 def find_first_big_number(arr):
     for idx, val in enumerate(arr):
         if val >= 10:
@@ -15,10 +16,23 @@ def find_first_big_number(arr):
 
 
 @test_runner(
-    expected=[2, -1, 0], assert_is_array=True, message="find_first_big_number test"
+    expected=[2, -1, 0], assert_is_array=True, message="Finding bing number's index"
 )
 def test_find_first_big_number(arr):
     return find_first_big_number(arr)
+
+
+# level 2
+def count_increasing_pairs(arr):
+    # Implement this
+    pass
+
+
+@test_runner(
+    expected=[3, 0, 1], assert_is_array=True, message="Counting increasing pairs"
+)
+def test_count_increasing_pairs(arr):
+    return count_increasing_pairs(arr)
 
 
 if __name__ == "__main__":
@@ -27,5 +41,13 @@ if __name__ == "__main__":
             [2, 5, 11, 3],  # 11 is at index 2
             [1, 4, 6, 9],  # no number >= 10, return -1
             [10, 20, 30],  # 10 is at index 0
+        ]
+    )
+
+    test_count_increasing_pairs(
+        [
+            [1, 3, 2, 4, 6],  # (1,3), (2,4), (4,6) → 3
+            [9, 7, 5, 3],  # strictly decreasing → 0
+            [5, 5, 6],  # only (5,6) → 1
         ]
     )
