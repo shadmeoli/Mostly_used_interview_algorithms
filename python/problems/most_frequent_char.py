41ca7611ead6bcbc5a13e2b7c6dcf7a9ea4104a0
shadmeoli <shadcodes@gmail.com>
2025-07-25 12:41:28 +0300

    update: frequency logger in a string

---
+++ python/problems/most_frequent_char.py
@@ -0,0 +1,43 @@
+"""
+Given a string, return the character that appears the most frequently.
+If multiple characters have the same highest frequency, return the one that appears first in the string.
+
+Example:
+Input: "abcaabcc"
+Output: "a"  # 'a' appears 3 times, which is the highest
+"""
+
+from collections import Counter
+
+from python.tester.testLogger import test_runner
+
+
+def most_frequent_char(s):
+    freq = Counter(s)
+    max_val = max(list(freq.values()))
+    ans = ""
+
+    for key, value in freq.items():
+        if value == max_val:
+            ans = key
+            break
+    return ans
+
+
+@test_runner(
+    expected=["a", "l", "e"],
+    assert_is_array=True,
+    message="Most frequent character",
+)
+def test_most_frequent_char(arr):
+    return most_frequent_char(arr)
+
+
+if __name__ == "__main__":
+    test_most_frequent_char(
+        [
+            "abcaabcc",  # 'a' appears 3 times
+            "levelupskills",  # 'l' appears 3 times
+            "edgecase",  # 'e' appears 2 times first
+        ]
+    )
