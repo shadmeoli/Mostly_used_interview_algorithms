538b8eab3eb16acdce82dd3d9788cc8664a2d006
shadmeoli <shadcodes@gmail.com>
2025-09-10 18:47:33 +0300

    feat: finding duplicates with list comprehention and constant space but only creating a single new array instead of two

---
+++ python/problems/duplicates.py
@@ -0,0 +1,17 @@
+"""
+Write a function that finds duplicates in an
+array and returns the duplicatre value.
+"""
+
+
+def findDuplicate(arr: [int]):
+    # dups = []
+    # for idx, i in enumerate(arr):
+    #     if i in arr[:idx]:
+    #         dups.append(i)
+    #     print(arr[:idx])
+    return [i for idx, i in enumerate(arr) if i in arr[:idx]]
+
+
+if __name__ == "__main__":
+    print(findDuplicate([1, 2, 3, 2, 4, 3, 4]))
